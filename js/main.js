dcce530028743bed37b99b4332ded013c2a654c1
Dorcimorci <dorottya1203@gmail.com>
2022-05-10 22:18:02 +0200

    test out the first things in JS

---
+++ js/main.js
@@ -0,0 +1,23 @@
+let price = 5;
+console.log(price);
+console.log(typeof price);
+
+price = "Hello";
+console.log(price);
+console.log(typeof price);
+
+price = "true";
+console.log(price);
+console.log(typeof price);
+
+price = true;
+console.log(price);
+console.log(typeof price);
+
+let name = "almafa";
+console.log(name);
+
+console.log(name.replace("a", "b"));
+console.log(name);
+
+console.log(name.replaceAll("a", "b"));
